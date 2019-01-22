95a5ada751e44ebabdb561223d5d4790cfb7fb32
ajfrierson <alvinfrierson@fuller.edu>
2019-01-22 15:50:34 -0800

    redid Movie.js code because of apparent bugs.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -26,8 +26,9 @@ export default class Movie extends Component {
         console.error(error);
       });
   };
-   // Uncomment this code when you're ready for the stretch problems
-   componentWillReceiveProps(newProps){
+
+  // Uncomment this code when you're ready for the stretch problems
+  componentWillReceiveProps(newProps){
     if(this.props.match.params.id !== newProps.match.params.id){
       this.fetchMovie(newProps.match.params.id);
     }
